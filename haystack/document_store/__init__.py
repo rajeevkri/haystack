438f801282c33fd63e5d7dd0e111c04cadfb6da4
Rajeev krishna singh <rajeevkrishna.singh@nslhub.com>
2021-12-08 11:06:32 +0530

    keeping only milvus 2 removing milvus 1

--- haystack/document_store/__init__.py
+++ haystack/document_store/__init__.py
@@ -1,7 +1,7 @@
 from haystack.document_store.elasticsearch import ElasticsearchDocumentStore, OpenDistroElasticsearchDocumentStore, OpenSearchDocumentStore
 from haystack.document_store.faiss import FAISSDocumentStore
 from haystack.document_store.memory import InMemoryDocumentStore
-from haystack.document_store.milvus import MilvusDocumentStore
+# from haystack.document_store.milvus import MilvusDocumentStore
 from haystack.document_store.milvus2x import Milvus2DocumentStore
 from haystack.document_store.sql import SQLDocumentStore
 from haystack.document_store.weaviate import WeaviateDocumentStore
