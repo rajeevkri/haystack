73643c659df633d62889ee7f8bc66501b448998d
Rajeev krishna singh <rajeevkrishna.singh@nslhub.com>
2021-12-21 10:45:32 +0530

    adding logs

--- haystack/pipeline.py
+++ haystack/pipeline.py
@@ -286,6 +286,8 @@ class Pipeline(BasePipeline):
                 try:
                     logger.debug(f"Running node `{node_id}` with input `{node_input}`")
                     logger.info(f" value of self.graph.nodes ==> `{self.graph.nodes}`") # adding to check will remove this
+                    logger.info(f" value of self.graph.nodes ==> `{self.graph.nodes[node_id]}`") # adding to check will remove this
+                    logger.info(f" value of self.graph.nodes ==> `{self.graph.nodes[node_id]["component"]}`") # adding to check will remove this
                     node_output, stream_id = self.graph.nodes[node_id]["component"]._dispatch_run(**node_input)
                 except Exception as e:
                     tb = traceback.format_exc()
